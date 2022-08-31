43a230a3541e58108982868738b004d782fc7257
Mika Tuupola <tuupola@appelsiini.net>
2022-08-31 17:47:37 +0700

    Add missing test for closure binding (#66)

--- tests/CorsTest.php
+++ tests/CorsTest.php
@@ -445,7 +445,7 @@ class CorsTest extends TestCase
             "credentials" => true,
             "cache" => 86400,
             "error" => function ($request, $response, $arguments) {
-                $response->getBody()->write("Error");
+                $response->getBody()->write(get_class($this));
                 return $response;
             }
         ]);
@@ -457,7 +457,7 @@ class CorsTest extends TestCase
 
         $response = $cors($request, $response, $next);
         $this->assertEquals(401, $response->getStatusCode());
-        $this->assertEquals("Error", $response->getBody());
+        $this->assertEquals(get_class($cors), $response->getBody());
     }
 
     public function testShouldCallInvokableErrorClass(): void
