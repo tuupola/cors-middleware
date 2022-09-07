aed532e0dbfeb911a84f7e88466ca8552958e8b9
Mika Tuupola <tuupola@appelsiini.net>
2022-09-07 13:13:29 +0300

    Configure and apply ECS DOCBLOCK set

--- ecs.php
+++ ecs.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([__DIR__ . "/src", __DIR__ . "/tests"]);
@@ -20,12 +22,18 @@ return static function (ECSConfig $ecsConfig): void {
 
     $ecsConfig->skip([
         // ClassAttributesSeparationFixer::class,
+        NoSuperfluousPhpdocTagsFixer::class,
     ]);
 
     $ecsConfig->sets([
         SetList::SPACES,
         SetList::ARRAY,
-        // SetList::DOCBLOCK,
+        SetList::DOCBLOCK,
         // SetList::PSR_12,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(PhpdocLineSpanFixer::class, [
+        "property" => "single",
+        "const" => "single",
     ]);
 };
